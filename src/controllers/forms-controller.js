a9e199b8348361e2679b76f92d963fba8218ac22
thfarina <thalitafarina3@gmail.com>
2021-08-29 15:19:48 -0300

    saius

--- src/controllers/forms-controller.js
+++ src/controllers/forms-controller.js
@@ -7,7 +7,7 @@ const Forms = mongoose.model('Forms');
 exports.listForms = async (req, res) => {
   try {
     const data = await Forms.find();
-    res.status(200).send(data);
+    res.status(200).send({data});
   } catch (e) {
     res.status(500).send({message: 'Falha ao carregar respostas do formulário.'});
   }
